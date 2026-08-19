985fc9b572b5d162621ce7c86a1ed8952a7bf853
lemon6243 <93980574+lemon6243@users.noreply.github.com>
2026-08-19 14:02:01 +0900

    Update ZonePanel.jsx

--- src/components/ZonePanel.jsx
+++ src/components/ZonePanel.jsx
@@ -9,7 +9,8 @@ import { useAdjacencyMap } from "../utils/dataLoader";
 import { useV2Data } from "../utils/dataLoaderV2";
 import { checkAdjacency, checkDowntown } from "../utils/validator";
 import { exportZonesCSV } from "../utils/scenarioManager";
-import CenterAllocationPanel from "./CenterAllocationPanel";
+import CenterAllocationCard from "./CenterAllocationPanel";
+
 
 
 
